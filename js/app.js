2c1be1569c47cae113e46a9c0ae775bff16e94e3
Luis Gonçalves <luischgoncalves@hotmail.com>
2022-10-27 11:41:34 +0100

    clean code

--- js/app.js
+++ js/app.js
@@ -130,5 +130,3 @@ const list = (containerId) => {
       containerUsers.appendChild(error);
     });
 };
-
-export default list;
--- js/tests/app.test.js
+++ js/tests/app.test.js
@@ -1,13 +1,5 @@
-import list from "../app";
-
 describe("Should render javascript exercise", () => {
-  test("should return an array of posts and users", () => {
-    // return letter.get().then(([usersData, postsData]) => {
-    //   // expect(usersData, postsData).toBe([]);
-    // });
-  });
+  test("should return an array of posts and users", () => {});
 
-  test("should return new array of posts and users and print it", () => {
-    // expect(list).toBe([]);
-  });
+  test("should return new array of posts and users and print it", () => {});
 });
